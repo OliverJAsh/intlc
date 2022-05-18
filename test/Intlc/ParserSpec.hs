549f9b55e904c2ae90a8930afa58a3ca62a94b76
Oliver Joseph Ash <oliverjash@gmail.com>
2022-05-18 16:13:53 +0100

    WIP

--- test/Intlc/ParserSpec.hs
+++ test/Intlc/ParserSpec.hs
@@ -1,12 +1,12 @@
 module Intlc.ParserSpec (spec) where
 
-import           Intlc.ICU
-import           Intlc.Parser
-import           Prelude               hiding (ByteString)
-import           Test.Hspec
-import           Test.Hspec.Megaparsec hiding (initialState)
-import           Text.Megaparsec       (ParseErrorBundle, runParser)
-import           Text.Megaparsec.Error (ErrorFancy (ErrorCustom))
+import Intlc.ICU
+import Intlc.Parser
+import Test.Hspec
+import Test.Hspec.Megaparsec hiding (initialState)
+import Text.Megaparsec (ParseErrorBundle, runParser)
+import Text.Megaparsec.Error (ErrorFancy (ErrorCustom))
+import Prelude hiding (ByteString)
 
 parseWith :: ParserState -> Parser a -> Text -> Either (ParseErrorBundle Text MessageParseErr) a
 parseWith s p = runParser (runReaderT p s) "test"
@@ -16,6 +16,29 @@ parse = parseWith initialState
 
 spec :: Spec
 spec = describe "parser" $ do
+  describe "TAST" $ do
+    it "plaintext" $ do
+      runParser plaintextParser "test" "hello" `shouldBe` Right (TPlaintext "hello")
+
+    it "interpolation" $ do
+      runParser interpolationParser "test" "{name}" `shouldBe` Right (TInterpolation "name")
+
+    it "node (interpolation)" $ do
+      runParser tnodeParser "test" "{name}" `shouldBe` Right (TInterpolation "name")
+
+    it "node (plaintext)" $ do
+      runParser tnodeParser "test" "hello" `shouldBe` Right (TPlaintext "hello")
+
+    it "TAST" $ do
+      runParser tastParser "test" "hello {name}, you are {years} years old"
+        `shouldBe` Right
+          [ TPlaintext "hello ",
+            TInterpolation "name",
+            TPlaintext ", you are ",
+            TInterpolation "years",
+            TPlaintext " years old"
+          ]
+
   describe "message" $ do
     it "does not tolerate unclosed braces" $ do
       parse msg `shouldFailOn` "a { b"
@@ -26,48 +49,65 @@ spec = describe "parser" $ do
     it "does not tolerate empty braces" $ do
       parse msg `shouldFailOn` "a {} b"
 
+    -- it "does not tolerate empty braces" (do
+    --   parse msg `shouldFailOn` "a {} b")
+
     it "does not tolerate empty tags" $ do
       parse msg `shouldFailOn` "a <> b"
 
     describe "plural hash" $ do
       it "parses as plaintext outside of plurals" $ do
         parse msg "#" `shouldParse` Static "#"
-        parse msg "{x, select, y {#}}" `shouldParse`
-          (Dynamic . pure . Interpolation . Arg "x" $
-            Select (pure $ SelectCase "y" (pure $ Plaintext "#")) Nothing)
+        parse msg "{x, select, y {#}}"
+          `shouldParse` ( Dynamic . pure . Interpolation . Arg "x" $
+                            Select (pure $ SelectCase "y" (pure $ Plaintext "#")) Nothing
+                        )
 
       it "parses as arg inside shallow plural" $ do
         let n = pure . Interpolation $ Arg "n" PluralRef
-        parse msg "{n, plural, one {#} other {#}}" `shouldParse`
-          (Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
-            RulePlural (pure $ PluralCase One n) (PluralWildcard n))
+        parse msg "{n, plural, one {#} other {#}}"
+          `shouldParse` ( Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
+                            RulePlural (pure $ PluralCase One n) (PluralWildcard n)
+                        )
 
       it "parses as nearest arg inside deep plural" $ do
         let n = pure . Interpolation $ Arg "n" PluralRef
         let i = pure . Interpolation $ Arg "i" PluralRef
-        parse msg "{n, plural, one {{i, plural, one {#} other {#}}} other {#}}" `shouldParse`
-          (Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
-            RulePlural (pure $ PluralCase One (
-              pure . Interpolation . Arg "i" . Plural . Cardinal $
-                RulePlural (pure $ PluralCase One i) (PluralWildcard i)
-            )) (PluralWildcard n))
+        parse msg "{n, plural, one {{i, plural, one {#} other {#}}} other {#}}"
+          `shouldParse` ( Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
+                            RulePlural
+                              ( pure $
+                                  PluralCase
+                                    One
+                                    ( pure . Interpolation . Arg "i" . Plural . Cardinal $
+                                        RulePlural (pure $ PluralCase One i) (PluralWildcard i)
+                                    )
+                              )
+                              (PluralWildcard n)
+                        )
 
       it "parses as arg nested inside other interpolation" $ do
         let n = pure . Interpolation $ Arg "n" PluralRef
-        parse msg "{n, plural, one {<f>#</f>} other {#}}" `shouldParse`
-          (Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
-            RulePlural (pure $ PluralCase One (
-              pure . Interpolation . Arg "f" . Callback $ n
-            )) (PluralWildcard n))
+        parse msg "{n, plural, one {<f>#</f>} other {#}}"
+          `shouldParse` ( Dynamic . pure . Interpolation . Arg "n" . Plural . Cardinal $
+                            RulePlural
+                              ( pure $
+                                  PluralCase
+                                    One
+                                    ( pure . Interpolation . Arg "f" . Callback $ n
+                                    )
+                              )
+                              (PluralWildcard n)
+                        )
 
     describe "escaping" $ do
       it "escapes non-empty contents between single quotes" $ do
-        parse msg "These are not interpolations: '{word1} {word2}'" `shouldParse`
-          Static "These are not interpolations: {word1} {word2}"
-        parse msg "'<notATag>hello</notATag>'" `shouldParse`
-          Static "<notATag>hello</notATag>"
-        parse msg "a {b} '{c}' {d} e" `shouldParse`
-          Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " {c} ", Interpolation (Arg "d" String), Plaintext " e"])
+        parse msg "These are not interpolations: '{word1} {word2}'"
+          `shouldParse` Static "These are not interpolations: {word1} {word2}"
+        parse msg "'<notATag>hello</notATag>'"
+          `shouldParse` Static "<notATag>hello</notATag>"
+        parse msg "a {b} '{c}' {d} e"
+          `shouldParse` Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " {c} ", Interpolation (Arg "d" String), Plaintext " e"])
         parse msg "'<f>'" `shouldParse` Static "<f>"
         parse msg "'<f>x</f>'" `shouldParse` Static "<f>x</f>"
         parse msg "'<f>x</g>'" `shouldParse` Static "<f>x</g>"
@@ -75,15 +115,15 @@ spec = describe "parser" $ do
       it "escapes next syntax character following one unclosed single quote" $ do
         parse msg "This is not an interpolation: '{word}" `shouldParse` Static "This is not an interpolation: {word}"
         parse msg "'<notATag>" `shouldParse` Static "<notATag>"
-        parse msg "a {b} '{c} {d} e" `shouldParse`
-          Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " {c} ", Interpolation (Arg "d" String), Plaintext " e"])
-        parse msg "a {b} 'c {d} e" `shouldParse`
-          Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " 'c ", Interpolation (Arg "d" String), Plaintext " e"])
+        parse msg "a {b} '{c} {d} e"
+          `shouldParse` Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " {c} ", Interpolation (Arg "d" String), Plaintext " e"])
+        parse msg "a {b} 'c {d} e"
+          `shouldParse` Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " 'c ", Interpolation (Arg "d" String), Plaintext " e"])
 
       it "escapes two single quotes as one single quote" $ do
         parse msg "This '{isn''t}' obvious." `shouldParse` Static "This {isn't} obvious."
-        parse msg "a {b} ''{c}'' {d} e" `shouldParse`
-          Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " '", Interpolation (Arg "c" String), Plaintext "' ", Interpolation (Arg "d" String), Plaintext " e"])
+        parse msg "a {b} ''{c}'' {d} e"
+          `shouldParse` Dynamic (Plaintext "a " :| [Interpolation (Arg "b" String), Plaintext " '", Interpolation (Arg "c" String), Plaintext "' ", Interpolation (Arg "d" String), Plaintext " e"])
 
       it "ignores one single quote not immediately preceding a syntax character" $ do
         parse msg "'" `shouldParse` Static "'"
@@ -125,8 +165,8 @@ spec = describe "parser" $ do
 
   describe "callback" $ do
     it "parses nested" $ do
-      parse callback "<f><g>x{y}z</g></f>" `shouldParse`
-        Arg "f" (Callback [Interpolation $ Arg "g" (Callback [Plaintext "x", Interpolation (Arg "y" String), Plaintext "z"])])
+      parse callback "<f><g>x{y}z</g></f>"
+        `shouldParse` Arg "f" (Callback [Interpolation $ Arg "g" (Callback [Plaintext "x", Interpolation (Arg "y" String), Plaintext "z"])])
 
     it "validates closing tag name" $ do
       parse callback "<hello></hello>" `shouldParse` Arg "hello" (Callback [])
@@ -156,13 +196,13 @@ spec = describe "parser" $ do
       parse cardinalPluralCases `shouldSucceedOn` "one {foo} other {bar}"
 
     it "requires a wildcard if there are any rule cases" $ do
-      parse cardinalPluralCases `shouldFailOn`    "=0 {foo} one {bar}"
+      parse cardinalPluralCases `shouldFailOn` "=0 {foo} one {bar}"
       parse cardinalPluralCases `shouldSucceedOn` "=0 {foo} one {bar} other {baz}"
       parse cardinalPluralCases `shouldSucceedOn` "=0 {foo} =1 {bar}"
 
     it "parses literal and plural cases, wildcard, and interpolation token" $ do
-      parseWith (ParserState (Just "xyz")) cardinalPluralCases "=0 {foo} few {bar} other {baz #}" `shouldParse`
-        Cardinal (MixedPlural (pure $ PluralCase (PluralExact "0") [Plaintext "foo"]) (pure $ PluralCase Few [Plaintext "bar"]) (PluralWildcard [Plaintext "baz ", Interpolation (Arg "xyz" PluralRef)]))
+      parseWith (ParserState (Just "xyz")) cardinalPluralCases "=0 {foo} few {bar} other {baz #}"
+        `shouldParse` Cardinal (MixedPlural (pure $ PluralCase (PluralExact "0") [Plaintext "foo"]) (pure $ PluralCase Few [Plaintext "bar"]) (PluralWildcard [Plaintext "baz ", Interpolation (Arg "xyz" PluralRef)]))
 
   describe "selectordinal" $ do
     it "disallows wildcard not at the end" $ do
@@ -178,12 +218,12 @@ spec = describe "parser" $ do
       parse ordinalPluralCases `shouldSucceedOn` "one {foo} two {bar} other {baz}"
 
     it "requires a wildcard" $ do
-      parse ordinalPluralCases `shouldFailOn`    "=0 {foo} one {bar}"
+      parse ordinalPluralCases `shouldFailOn` "=0 {foo} one {bar}"
       parse ordinalPluralCases `shouldSucceedOn` "=0 {foo} one {bar} other {baz}"
 
     it "parses literal and plural cases, wildcard, and interpolation token" $ do
-      parseWith (ParserState (Just "xyz")) cardinalPluralCases "=0 {foo} few {bar} other {baz #}" `shouldParse`
-        Cardinal (MixedPlural (pure $ PluralCase (PluralExact "0") [Plaintext "foo"]) (pure $ PluralCase Few [Plaintext "bar"]) (PluralWildcard [Plaintext "baz ", Interpolation (Arg "xyz" PluralRef)]))
+      parseWith (ParserState (Just "xyz")) cardinalPluralCases "=0 {foo} few {bar} other {baz #}"
+        `shouldParse` Cardinal (MixedPlural (pure $ PluralCase (PluralExact "0") [Plaintext "foo"]) (pure $ PluralCase Few [Plaintext "bar"]) (PluralWildcard [Plaintext "baz ", Interpolation (Arg "xyz" PluralRef)]))
 
   describe "select" $ do
     it "disallows wildcard not at the end" $ do
